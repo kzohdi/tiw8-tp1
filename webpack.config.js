559bf2eadd09688d43dcce5582400a41fe1d134d
ZOHDI KARIM p1926304 <karim.zohdi@etu.univ-lyon1.fr>
2019-10-11 10:21:06 +0200

    Configuration de Babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
         ]
     }
-}
+};
